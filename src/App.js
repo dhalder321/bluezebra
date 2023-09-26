bb1945cabf3f01e9b089c970c8fd1a38148bfb40
dhalder321 <debjit.halder.bbsr@gmail.com>
2023-09-25 23:45:23 -0400

    added note and navbar links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 
 import React from 'react';
+import TrafficDetails from './components/TrafficDetails';
+import Note from './components/Note';
 
 import ZNavbar from './components/ZNavbar';
 //import Main from './components/Main';
@@ -13,8 +15,10 @@ function App() {
   return (
     <div>
         <ZNavbar></ZNavbar>
+        <Note text="This site is still under development. If you do not find any materials, please drop a note in the suggestion box by clicking on Suggestion/Comments link from top right corner. We shall get back to you as soon as possible with the materials."></Note>
         <Main></Main>
       <Footer></Footer>
+      <TrafficDetails></TrafficDetails>
     </div>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -8,8 +8,17 @@ export default function Main(){
 
     //alert('Main-No: ' + Math.random(1000));
     const [selectedTopicId, setSelectedTopicId] = React.useState("0");
+    const [criteriaData, setCriteriaData] = React.useState({
+        "selectedLanguage": "-1",
+        "selectedAge" : "-1",
+        "selectedGrade": "-1",
+        "selectedMaterialType": "-1",
+        "selectedPurpose": "-1"
+    });
     const [allTopics, setAllTopics] = React.useState([]);
+    const [refreshPage, setRefreshPage] = React.useState(true);
 
+    var criteriaChanged = false;
 
     React.useEffect(() => {
         fetch("https://iz4tu2qmv5.execute-api.us-east-2.amazonaws.com/Test/topics")
@@ -17,6 +26,24 @@ export default function Main(){
             .then(data => setAllTopics(data.Topics));
             //alert("useEffect-No::" + Math.random(1000));
       },[]);
+
+
+    const handleCriteriaChange = (e)=>{
+        e && e.preventDefault();
+        setCriteriaData({
+            ...criteriaData,
+            [e.target.id]: e.target.value
+          });
+        criteriaChanged = true;
+    };
+
+    const applyCriteria = (e)=>{
+        e && e.preventDefault();
+        if(criteriaChanged)
+        {
+            setRefreshPage(!refreshPage);
+        }
+    };
 
     return (
     <>
@@ -29,10 +56,10 @@ export default function Main(){
                 value={selectedTopicId}
                 onChange={(e) =>
                     {
+                        e && e.preventDefault();
                         const {value} = e.target
                         setSelectedTopicId(value);
-                        console.log(value)
-                        return
+                        //alert("Topic id:" + value)
                     }
                 }>
                 <option value="defaultSelection" key="0">---Select a topic---</option>
@@ -49,8 +76,10 @@ export default function Main(){
                 className="submitButton" 
                 onClick={(e) =>
                     {
+                        e && e.preventDefault();
                         const {value} = e.target
                         setSelectedTopicId(value);
+                        setRefreshPage(!refreshPage);
                         console.log(value)
                         return
                     }
@@ -58,47 +87,78 @@ export default function Main(){
             }>Get Materials</button>
             
             <div className="selectionBox">
-                <h4> Filter conditions</h4>
-                <select className="selectionDropdown">
-                    <option>--Select Language---</option>
-                    <option>English - USA</option>
-                    <option>Spanish - USA</option>
-                    <option>French - France</option>
-                    <option>Portuguese - Brazil</option>
+                <h4>Filter conditions</h4>
+                <select 
+                className="selectionDropdown" 
+                id="selectedLanguage"
+                value={criteriaData.selectedLanguage}
+                onChange={handleCriteriaChange}>
+                    <option value="-1">--Select Language---</option>
+                    <option value="en-US">English - USA</option>
+                    <option value="es-US">Spanish - USA</option>
+                    <option value="fr-FR">French - France</option>
+                    <option value="pt-BR">Portuguese - Brazil</option>
                 </select>
-                <select className="selectionDropdown">
-                    <option>--Select Age---</option>
-                    <option>5+</option>
-                    <option>8+</option>
-                    <option>12+</option>
-                    <option>Adult</option>
+                <select 
+                className="selectionDropdown" 
+                id="selectedAge"
+                value={criteriaData.selectedAge}
+                onChange={handleCriteriaChange}>
+                    <option value="-1">--Select Age---</option>
+                    <option value="5+">5+</option>
+                    <option value="8+">8+</option>
+                    <option value="teen">Teeneger</option>
+                    <option value="adult">Adult</option>
                 </select>
-                <select className="selectionDropdown">
-                    <option>--Select Grade---</option>
-                    <option>5th to 8th</option>
-                    <option>8th to 12th</option>
-                    <option>12+</option>
-                    <option>College and Beyond</option>
+                <select 
+                className="selectionDropdown" 
+                id="selectedGrade"
+                value={criteriaData.selectedGrade}
+                onChange={handleCriteriaChange}>
+                    <option value="-1">--Select Grade---</option>
+                    <option value="5+">5th to 8th</option>
+                    <option value="8+">8th to 12th</option>
+                    <option value="12+">12+</option>
+                    <option value="beyond">College and Beyond</option>
                 </select>
-                <select className="selectionDropdown">
-                    <option>--Select Material Types---</option>
-                    <option>Text</option>
-                    <option>Comic Version</option>
-                    <option>Audio Only</option>
-                    <option>Videos</option>
-                    <option>Quiz</option>
-                    <option>Question Papers</option>
+                <select 
+                className="selectionDropdown" 
+                id="selectedMaterialType"
+                value={criteriaData.selectedMaterialType}
+                onChange={handleCriteriaChange}>
+                    <option value="-1">--Select Material Types---</option>
+                    <option value="text">Text</option>
+                    <option value="comic">Comic Version</option>
+                    <option value="audio">Audio Only</option>
+                    <option value="video">Videos</option>
+                    <option value="quiz">Quiz</option>
+                    <option value="questionPaper">Question Papers</option>
                 </select>
-                <select className="selectionDropdown">
-                    <option>--Select Purpose---</option>
-                    <option>Leisure Reading</option>
-                    <option>Knowledge Gain</option>
-                    <option>Academic</option>
-                    <option>Research</option>
+                <select 
+                className="selectionDropdown" 
+                id="selectedPurpose"
+                value={criteriaData.selectedPurpose}
+                onChange={handleCriteriaChange}>
+                    <option value="-1">--Select Purpose---</option>
+                    <option value="leisure">Leisure Reading</option>
+                    <option value="knowledge">Knowledge Gain</option>
+                    <option value="academic">Academic</option>
+                    <option value="research">Research</option>
                 </select>
+                <button 
+                type="submit"
+                className="submitButtonShow" 
+                disabled
+                onClick={applyCriteria}>Apply</button>
             </div> 
             <section className="material--box">
-                <Material selectedTopicId={selectedTopicId}></Material>
+                <Material selectedTopicId={selectedTopicId} 
+                    selectedLanguage={criteriaData.selectedLanguage}
+                    selectedAge={criteriaData.selectedAge} 
+                    selectedGrade={criteriaData.selectedGrade} 
+                    selectedMaterialType= {criteriaData.selectedMaterialType} 
+                    selectedPurpose={criteriaData.selectedPurpose}>
+                </Material>
             </section>
         </div>
 
